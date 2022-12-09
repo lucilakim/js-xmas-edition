3cf4a0d1eb197b0d7a94e105cff8e8565fdfe7ae
Lucila Kim <lucila.kim.9@gmail.com>
2022-12-09 13:27:33 -0300

    Fix functions, adding return word. And separate the functions in another file --> class-7-task-calculations.js

--- tarea/class-7-task.js
+++ tarea/class-7-task.js
@@ -2,29 +2,16 @@ const $form = document.querySelector('[name=formulario]');
 const $button = $form.submit;
 
 $button.onclick = (e) => {
+    e.preventDefault();
     const city = $form.ciudad.value;
     const giftDescription = $form['descripcion-regalo'].value;
-    
-    console.log(validateCity(city));
-    console.log(validateGiftDescription(giftDescription));
-    e.preventDefault();
+
+    console.log(city);
+    console.log(giftDescription)
 }
 
 //---------- Functions will go in another file called "class-7-task-calculations.js".
-function validateCity(city) {
-    ValidateNotEmpty(city, 'You must select a city');
-} 
 
-function validateGiftDescription(description){
-    ValidateNotEmpty(description, 'You must write what you would like to receive')
-}
-
-function ValidateNotEmpty(value, message) {
-    if (value.length === 0) {
-        return message;
-    }
-    return '';
-}
 /*
 * Hacer las funciones de validación de validarCiudad y validarDescripcionRegalo.
 * Escribir pruebas para esas funciones.
