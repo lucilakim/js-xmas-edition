60e066672cc4c4d57e4124672b7dcbc90f090262
Lucila Kim <lucila.kim.9@gmail.com>
2022-12-10 14:55:30 -0300

    Fix error in test file, onclick was removed

--- tarea/class-7-task-tests.js
+++ tarea/class-7-task-tests.js
@@ -3,18 +3,14 @@ function validateNotEmptyTest() {
         validateNotEmpty('') === validateNotEmpty.message,
         'Validate that is not empty, not valid that it is not empty.'
     )
-
-    $button.onclick = (e) => {
-        e.preventDefault();
-        console.assert(
-            validateCity('') === 'You must select a city',
-            'Validate city not valid that city field is not empty'
-        )
-        console.assert(
-            validateGiftDescription('') === 'You must write what you would like to receive',
-            'Validate gift description not valid that description field is not empty'
-        )
-    }
+    console.assert(
+        validateCity('') === 'You must select a city',
+        'Validate city not valid that city field is not empty'
+    )
+    console.assert(
+        validateGiftDescription('') === 'You must write what you would like to receive',
+        'Validate gift description not valid that description field is not empty'
+    )
 }
 
 validateNotEmptyTest();
